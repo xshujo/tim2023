9255c07b35643b0a47a042da9c7c60e912e35653
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-09-20 11:43:22 -0400

    Modifications loader partie 4

--- functions.php
+++ functions.php
@@ -111,6 +111,7 @@ add_action('pre_get_posts', 'cidweb_modifie_requete_principal');
 // Loader
 function custom_loading_script() {
     wp_enqueue_script('loader', get_template_directory_uri() . './loader.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('loader', get_template_directory_uri() . './loader.css', array('css'), '1.0', true);
     
   }
   
