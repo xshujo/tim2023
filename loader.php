7721d295ad3d8b7aa5e5beea6a6ae3d78cd33583
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-09-20 09:24:25 -0400

    Ajout fichiers medias et loader

---
+++ loader.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Site Web Tim</title>
+</head>
+<body class="fond_loader">
+    <header>
+        
+    </header>
+    <main>
+        
+        <div style="text-align:center">
+            <img class="img_1" src="./medias/logotimbleu.png" alt="">
+            <img class="img_2" src="./medias/logotimrouge.png" alt="">
+            <img class="img_3" src="./medias/logotimvert.png" alt="">
+            <img class="img_4" src="./medias/logotimbleu.png" alt="">
+            <img class="img_5" src="./medias/logotimrouge.png" alt="">
+            <p>CHARGEMENT...</p>
+            <img class="img_6" src="./medias/logotimvert.png" alt="">
+        </div>
+
+     
+    </main>
+    <script>
+        setTimeout(function() {
+            window.location.href = "index.htm";
+        }, 6000); // Délais de 6 seconde pour le changement de page
+    </script>
+    <footer>
+        
+    </footer>
+</body>
+</html>
