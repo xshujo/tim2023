4debbb6cb3e3f0f6600070724dd3bdb044a20b12
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-09-20 11:10:23 -0400

    Modifications loader partie 3

--- front-page.php
+++ front-page.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
-<?php get_template_part('loader'); ?>
+
 <main>
+<div class="loader-tim">
+    <?php include('wp-content/plugins/loader/loader.html'); ?>
+</div>
     <h1>Test</h1>
+   
 </main>
 <?php get_footer(); ?>
