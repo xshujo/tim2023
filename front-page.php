e882af09af2d2ee97abc5fcaf8a09f6cf036a129
Taïsha Dorval-Mbele <137970027+xshujo@users.noreply.github.com>
2023-09-25 09:26:54 -0400

    Création d'une page 404. Erreurs mises en commentaire. Modification des classes du header.

--- front-page.php
+++ front-page.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 
 <main>
-<div class="loader-tim">
-    <?php include('wp-content/plugins/loader/loader.html'); ?>
-</div>
+    <div class="loader-tim">
+        <!-- <?php //include('wp-content/plugins/loader/loader.html'); 
+                ?> Pas le droit d'appeler un plugin comme ça -->
+    </div>
     <h1>Test</h1>
-   
+
 </main>
 <?php get_footer(); ?>
