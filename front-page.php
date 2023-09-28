d32939583fa7d8c306f6fde641fc1afd90ae5e6b
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-09-27 21:07:23 -0400

    Style front-page début

--- front-page.php
+++ front-page.php
@@ -7,30 +7,30 @@
 <?php get_header(); ?>
 <main class="contenu_accueil">
 
-<section>
+<section class="contenu1">
     <h1>CRÉATION</h1>
     <h1>INTÉRATION</h1>
     <h1>PROGRAMMATION</h1>
 </section>
 
-<section>
+<section class="contenu2">
     <h1>"ÉTUDIER EN TIM C'EST [...]" nom, cohorte</h1>
 
 </section>
 
-<section>
+<section class="contenu3">
     <h1>UNE ÉQUIPE D'ENSEINANTS DÉVOUÉS</h1>
 </section>
 
-<section>
+<section class="contenu4">
     <h1>DES COURS SELON VOS INTÉRÊTS</h1>
 </section>
 
-<section>
+<section class="contenu5">
     <h1>UN APPRENTISSAGE PAR PROJET</h1>
 </section>
 
-<section>
+<section class="contenu6">
     <h1>ENVIE DE VENIR ÉTUDIER?</h1>
     <h2>Soumets dès maintenant une demande d'admission</h2>
 </section>
