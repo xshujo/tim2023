ff8f02a24da33cf57667afa853bd856c1a8403c4
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-10-04 10:01:37 -0400

    Animation liens menu header

--- footer.php
+++ footer.php
@@ -16,10 +16,10 @@
 <section class="colonne3">
         <div class="c3">
             <h1>Suivez-nous</h1>
-            <a href="https://youtube.com/home"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=youtube&color=ff6b6b" width="32" height="32"></a>
-            <a href="https://www.instagram.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=ff6b6b" width="32" height="32"></a>
-            <a href="https://www.facebook.com" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=facebook&color=ff6b6b" width="32" height="32"></a>
-            <a href="https://discord.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=discord&color=ff6b6b" width="32" height="32"></a>
+            <a href="https://youtube.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=youtube&color=ffffff" width="32" height="32"></a>
+            <a href="https://www.instagram.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=ffffff" width="32" height="32"></a>
+            <a href="https://www.facebook.com" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=facebook&color=ffffff" width="32" height="32"></a>
+            <a href="https://discord.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=discord&color=ffffff" width="32" height="32"></a>
         </div>
 </section>
     
