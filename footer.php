3dc18d0d0ebd2d295ca9c378706e6aec16e6adcf
Taïsha Dorval-Mbele <137970027+xshujo@users.noreply.github.com>
2023-10-04 09:42:43 -0400

    Nettoyage de code, commentaires, ajout d'une description (meta)

--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
 </div>
 <footer>
-<section class="colonne1">
+    <section class="colonne1">
         <div class="c1">
             <h1>Adresse</h1>
             <p>3800 Sherbrooke St E, Montreal, Quebec H1X 2A2</p>
             <p>6220 Sherbrooke St E, Montreal, Quebec H1N 1C1</p>
         </div>
-</section>
-<section class="colonne2">
+    </section>
+    <section class="colonne2">
         <div class="c2">
             <h1>Contact</h1>
             <p>Téléphone:(514) 254-7131</p>
         </div>
-</section>
-<section class="colonne3">
+    </section>
+    <section class="colonne3">
         <div class="c3">
             <h1>Suivez-nous</h1>
-            <a href="https://youtube.com/home"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=youtube&color=ff6b6b" width="32" height="32"></a>
-            <a href="https://www.instagram.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=ff6b6b" width="32" height="32"></a>
+            <a href="https://youtube.com/home" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=youtube&color=ff6b6b" width="32" height="32"></a>
+            <a href="https://www.instagram.com" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=ff6b6b" width="32" height="32"></a>
             <a href="https://www.facebook.com" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=facebook&color=ff6b6b" width="32" height="32"></a>
-            <a href="https://discord.com"  target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=discord&color=ff6b6b" width="32" height="32"></a>
+            <a href="https://discord.com" target="_blank"><img src="https://s2.svgbox.net/social.svg?ic=discord&color=ff6b6b" width="32" height="32"></a>
         </div>
-</section>
-    
+    </section>
+
 </footer>
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -2,13 +2,15 @@
     <?php include('wp-content/themes/tim2023/loader/loader.php'); ?>
 </div> -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+    <!-- Description à ajuster -->
+    <meta name="description" content="Au cours de cette formation technique de 3 ans vous apprendrez à analyser, imaginer, réaliser et coordonner un projet multimédia - site Web, jeux.">
+    <!-- Importation des polices -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@800&family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400&display=swap" rel="stylesheet">
@@ -17,15 +19,15 @@
 
 <body>
     <header>
-    <section class="site__header__logo">
-         <?php the_custom_logo()?> 
-        <div class="menu-site">
-            <?php wp_nav_menu(array(
-                "menu" => "entete",
-                "container" => "nav"
+        <section class="site__header__logo">
+            <!-- Affichage du logo -->
+            <?php the_custom_logo() ?>
+            <!-- Affichage du menu d'entête -->
+            <div class="menu-site">
+                <?php wp_nav_menu(array(
+                    "menu" => "entete",
+                    "container" => "nav"
                 )) ?>
-            
-        </div>
-    </section>
-        
+            </div>
+        </section>
     </header>
