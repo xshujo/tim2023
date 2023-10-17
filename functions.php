289c1c89552ac0fd76406aeb4649be4ef067426d
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-16 23:21:08 -0400

    suppression fichier data genere-citations

--- functions.php
+++ functions.php
@@ -60,12 +60,3 @@ function custom_loading_script()
 }
 
 add_action('wp_enqueue_scripts', 'custom_loading_script');
-
-//fonction citation aleatoire
-function ajouter_script_citations()
-{
-    // wp_enqueue_script('citations-script', get_template_directory_uri() . './code/citations.js', array('javascript'), '1.0', true);
-    // wp_enqueue_script('citations-script', get_template_directory_uri() . './data/genere-citations.json', array('json'), '1.0', true);
-    wp_enqueue_script('script-name', get_template_directory_uri() . '/code/citations.js', array(), '1.0.0', true);
-}
-add_action('wp_enqueue_scripts', 'ajouter_script_citations');
