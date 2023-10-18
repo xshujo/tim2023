d27302289fce5381c48cc33d49656f7975c1fb31
ogregoryrivera <gregosorio24@gmail.com>
2023-10-18 11:43:24 -0400

    Ajustement CSS

--- front-page.php
+++ front-page.php
@@ -10,34 +10,34 @@
 <main class="contenu_accueil">
 
     <section class="video-section">
-        
+
         <video autoplay muted loop id="background-video">
             <source src="<?php echo get_template_directory_uri(); ?>/medias/loop.mp4" type="video/mp4">
-        
+
             Your browser does not support the video tag.
         </video>
-        
+
         <div class="contenu" id="contenu">
             <div class="conteneur-titre">
                 <h1 class="titre-accueil1">Création</h1>
                 <h1 class="titre-accueil2">Intégration</h1>
                 <h1 class="titre-accueil3">Programmation</h1>
             </div>
-                <div id="ligne" style="line-height: 1; color: white"></div>
-                <p id="texteIntro">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti
-                    temporibus quaerat, cumque dignissimos numquam.
-                </p>
+            <div id="ligne" style="line-height: 1; color: white"></div>
+            <p id="texteIntro">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corrupti
+                temporibus quaerat, cumque dignissimos numquam.
+            </p>
         </div>
-        
+
     </section>
 
-    <section class="contenu2"  id="contenu2">
+    <section class="contenu2" id="contenu2">
         <blockquote id="citation">Étudier en TIM c'est...</blockquote>
         <p id="auteur"> - Nom,cohorte</p>
 
         <div class="image_texte">
-            
+
             <img src="wp-content/themes/tim2023/medias/2048/Photo10_2048.jpg" alt="imgFrontPage" width="325" height="200" style="border-radius: 25px" />
             <div class="bordure_image"></div>
             <p>
@@ -49,14 +49,14 @@
                 magni deserunt minima!
             </p>
         </div>
-        
+
     </section>
 
-    
 
-    <section class="contenu3"  id="contenu">
+
+    <section class="contenu3" id="contenu">
         <h2>Une équipe de professeurs dévoués</h2>
-        
+
         <div class="image_texte">
             <img src="wp-content/themes/tim2023/medias/2048/ImageEquipeProf1_2048.jpg" alt="image des profs du TIM" width="325" height="200" style="border-radius: 25px" />
             <div class="bordure_image"></div>
@@ -69,11 +69,11 @@
                 </p>
                 <button class="bouton_prof">voir plus</button>
             </div>
-            
+
         </div>
     </section>
 
-    <section class="contenu4"  id="contenu2">
+    <section class="contenu4" id="contenu2">
         <h2>Des cours selon vos intérêts</h2>
         <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque
@@ -105,13 +105,13 @@
                 <img src="wp-content/themes/tim2023/medias/SVG/vr.svg" alt="" />
                 <h3>Réalité virtuelle</h3>
             </div>
-           
+
         </div>
 
-        <buttonm class="bouton_cours">voir plus</button>
+        <button class="bouton_cours">voir plus</button>
     </section>
 
-    <section class="contenu5"  id="contenu">
+    <section class="contenu5" id="contenu">
         <h2>Un apprentissage par projet</h2>
         <div class="image_texte">
             <img src="wp-content/themes/tim2023/medias/2048/Photo8_2048.jpg" alt="Une étudiante TIM qui travaille" width="325" height="200" style="border-radius: 25px" />
@@ -128,7 +128,7 @@
         </div>
     </section>
 
-    <section class="contenu6"  id="contenu2">
+    <section class="contenu6" id="contenu2">
         <h1>Envie de venir étudier?</h1>
         <h2>Soummets dès maintenant une demande d'admission</h2>
         <a href="https://admission.sram.qc.ca/"><button class="bouton_admission">Soumettre</button></a>
@@ -136,8 +136,7 @@
 
     </section>
 
-    <script>
-
+    <!-- <script>
         // Apparition gauche à droite des éléments
         const section = document.querySelectorAll('#contenu, #contenu2');
 
@@ -172,11 +171,11 @@
 
         // Scroll to the top of the page when the button is clicked
         scrollToTopBtn.addEventListener('click', () => {
-            document.documentElement.scrollIntoView({ behavior: 'smooth' });
+            document.documentElement.scrollIntoView({
+                behavior: 'smooth'
+            });
         });
-
-
-    </script>
+    </script> -->
 
 </main>
 <?php get_footer(); ?>
