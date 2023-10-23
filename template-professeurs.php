f135502dcb560584e705fe4a6a9ca98f322cb25a
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-23 02:47:49 -0400

    test affichage des articles

--- template-professeurs.php
+++ template-professeurs.php
@@ -6,8 +6,13 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <?php the_title(); ?>
+    <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
-
+    <article>
+        <?php
+        $post = get_post(13); // l'ID de l'article
+        echo $post->post_content; // Affiche le contenu de l'article
+        ?>
+    </article>
 </main>
 <?php get_footer(); ?>
