61dc884db99881b9577b7a3166eb4b3b8eeff379
Taïsha DM <xshujo@gmail.com>
2023-10-25 11:46:08 -0400

    Ajustement des styles du menu

--- template-professeurs.php
+++ template-professeurs.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template name: Professeurs
  */
@@ -8,21 +9,20 @@
 
 <main>
 
-<?php
-      $category = get_queried_object();
-      $args = array(
-         'category_name' => $category->slug,
-         'orderby' => 'title',
-         'order' => 'ASC'
-      );
-      $query = new WP_Query( $args );
-      if ( $query->have_posts() ) :
-         while ( $query->have_posts() ) : $query->the_post(); ?>
-            <!-- $category->slug peut avoir une de ces deux valeurs : "cours" ou "4w4" -->
-            <?php get_template_part('template-parts/professeur', $category->slug); ?>
-         <?php endwhile; ?>
-      <?php endif;
-      wp_reset_postdata();?>
+   <?php
+   $category = get_queried_object();
+   $args = array(
+      'category_name' => $category->slug,
+      'orderby' => 'title',
+      'order' => 'ASC'
+   );
+   $query = new WP_Query($args);
+   if ($query->have_posts()) :
+      while ($query->have_posts()) : $query->the_post(); ?>
+         <?php get_template_part('template-parts/professeur', $category->slug); ?>
+      <?php endwhile; ?>
+   <?php endif;
+   wp_reset_postdata(); ?>
 
 </main>
 
