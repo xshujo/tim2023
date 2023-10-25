7d2cfcce6732efd6853338e30c9af49fc7d64975
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-10-25 18:35:11 -0400

    Ajustements logo footer

--- footer.php
+++ footer.php
@@ -16,10 +16,11 @@
 <section class="colonne3">
         <div class="c3">
             <h1>Suivez-nous</h1>
-            <a href="https://www.youtube.com/@TIMaisonneuve/featured"  target="_blank"><img src="wp-content/themes/tim2023/medias/logo-rs/logo-youtube.png" width="32" height="32"></a>
-            <a href="https://www.instagram.com/maisonneuvetim/"  target="_blank"><img src="wp-content/themes/tim2023/medias/logo-rs/logo-instagram.png" width="32" height="32"></a>
-            <a href="https://www.facebook.com/groups/170302146254" target="_blank"><img src="wp-content/themes/tim2023/medias/logo-rs/logo-facebook.png" width="32" height="32"></a>
-            <a href="https://discord.com/invite/rZUr5X4dcF"  target="_blank"><img src="wp-content/themes/tim2023/medias/logo-rs/logo-discord.png" width="32" height="32"></a>
+            <a href="https://www.facebook.com/groups/170302146254" target="_blank"><img src="http://localhost/tim2023/wp-content/uploads/2023/10/logo-facebook.png" width="32" height="32"></a>
+            <a href="https://www.instagram.com/maisonneuvetim/"  target="_blank"><img src="http://localhost/tim2023/wp-content/uploads/2023/10/logo-instagram.png" width="32" height="32"></a>
+            <a href="https://www.linkedin.com/in/tim-coll%C3%A8ge-de-maisonneuve-9407b7131/"  target="_blank"><img src="http://localhost/tim2023/wp-content/uploads/2023/10/logo-linkedin.png" width="32" height="32"></a>
+            <a href="https://www.youtube.com/@TIMaisonneuve/featured"  target="_blank"><img src="http://localhost/tim2023/wp-content/uploads/2023/10/logo-youtube.png" width="32" height="32"></a>
+            <a href="https://discord.com/invite/rZUr5X4dcF"  target="_blank"><img src="http://localhost/tim2023/wp-content/uploads/2023/10/logo-discord.png" width="32" height="32"></a>
         </div>
 </section>
     
