0f5d7e87e54caf8049e4ad54b1cbc3b6dc77a1c3
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-26 19:00:37 -0400

    changement pour la galerie

--- template-professeurs.php
+++ template-professeurs.php
@@ -6,13 +6,12 @@
 ?>
 <?php get_header(); ?>
 <h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
-<main>
 
+<div class="galerie">
     <?php
     $args = array(
         'category_name' => 'professeurs',
-        'posts_per_page' => 1,
+        'posts_per_page' => 13,
         'orderby'        => 'title', // Tri par titre
         'order'          => 'ASC',   // Dans l'ordre alphabétique croissant
     );
@@ -21,12 +20,24 @@
     //Affichage des articles (descriptions des professeurs)
     if ($query->have_posts()) :
         while ($query->have_posts()) : $query->the_post();
-            the_content();
+            if (has_post_thumbnail()) {
+    ?>
+                <!--affiche images des articles-->
+                <div>
+                    <?php the_post_thumbnail('thumbnail');
+                    ?>
+                </div>
+    <?php
+            }
         endwhile;
         wp_reset_postdata();
     else :
         echo 'Aucun article trouvé dans cette catégorie.';
     endif;
     ?>
+</div>
+
+<main>
+
 </main>
 <?php get_footer(); ?>
