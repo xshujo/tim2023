e2b6a240dc2880e4bfb356e5a6558033c92d9535
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-26 16:45:04 -0400

    Affichage des articles (descriptions des profs)

--- template-professeurs.php
+++ template-professeurs.php
@@ -5,14 +5,26 @@
  */
 ?>
 <?php get_header(); ?>
+<h1><?php the_title(); ?></h1>
+<?php the_content(); ?>
 <main>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-    <article>
-        <?php
-        $post = get_post(13); // l'ID de l'article
-        echo $post->post_content; // Affiche le contenu de l'article
-        ?>
-    </article>
+    <?php
+    $args = array(
+        'category_name' => 'professeurs',
+        'posts_per_page' => 1,
+    );
+
+    $query = new WP_Query($args);
+    //Affichage des articles (descriptions des professeurs)
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            the_title();
+            the_content();
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'Aucun article trouvé dans cette catégorie.';
+    endif;
+    ?>
 </main>
 <?php get_footer(); ?>
