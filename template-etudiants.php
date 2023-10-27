19ba513308b60f04f17513d400f6a525cd23cc26
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-10-27 16:05:58 -0400

    Affichage des posts sur la page Vie étudiante

--- template-etudiants.php
+++ template-etudiants.php
@@ -6,7 +6,24 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <?php get_the_title(); ?>
-    <?php the_content(); ?>
+    <?php 
+        $category = get_queried_object();
+        $args = array(
+            'category_name' => 'Evenements',
+            'orderby' => 'title',
+            'order' => 'ASC'
+        );
+        $query = new WP_Query( $args );
+        if ( $query->have_posts() ) :
+            while ( $query->have_posts() ) : $query->the_post(); ?>
+                <?php get_the_title(); ?>
+                <?php the_content(); ?>
+                <?php if (has_post_thumbnail()) {
+                    the_post_thumbnail('thumbnail');
+                } ?>
+            <?php endwhile; ?>
+        <?php endif;
+        wp_reset_postdata();?>
+    ?>
 </main>
 <?php get_footer(); ?>
