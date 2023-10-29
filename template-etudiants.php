df7ccd1b9f631c8588b474fe6cbd7bc38064b69f
marieeve0916 <e2142821@cmaisonneuve.qc.ca>
2023-10-29 13:25:56 -0400

    Début du CSS de la page Vie étudiante + Séparation des événements et de la galerie en PHP

--- template-etudiants.php
+++ template-etudiants.php
@@ -1,11 +1,30 @@
 <?php
-
 /**
  * Template name: Vie etudiante
  */
 ?>
 <?php get_header(); ?>
 <main>
+    <!-- Affichage de la galerie Vie étudiante sur la page -->
+    <div class="zone_galerie">
+        <?php
+            $category = get_queried_object();
+            $args = array(
+                'category_name' => 'GalerieVieEtudiante'
+            );
+            $query = new WP_Query( $args );
+            if ( $query->have_posts() ) :
+                while ( $query->have_posts() ) : $query->the_post(); ?>
+                    <div class="article_galerie_vie_etudiante">
+                        <?php the_content(); ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif;
+        wp_reset_postdata();?>   
+    </div>
+
+    <!-- Affichage des événements sur la page -->
+    <div class="zone_evenements">
     <?php 
         $category = get_queried_object();
         $args = array(
@@ -14,14 +33,17 @@
         $query = new WP_Query( $args );
         if ( $query->have_posts() ) :
             while ( $query->have_posts() ) : $query->the_post(); ?>
-                <?php if (has_post_thumbnail()) {
-                    the_post_thumbnail('thumbnail');
-                } ?>
-                <?php get_the_title(); ?>
-                <?php the_title(); ?>
-                <?php the_content(); ?>
+                <div class="article_vie_etudiante">
+                    <?php if (has_post_thumbnail()) {
+                        the_post_thumbnail('thumbnail');
+                    } ?>
+                    <?php get_the_title(); ?>
+                    <?php the_title(); ?>
+                    <?php the_content(); ?>
+                </div>
             <?php endwhile; ?>
         <?php endif;
         wp_reset_postdata();?>
+    </div>
 </main>
 <?php get_footer(); ?>
