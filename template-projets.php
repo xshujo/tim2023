92490945c039fdb05dc0a1a604588b46c50f49d5
ogregoryrivera <gregosorio24@gmail.com>
2023-10-30 15:50:02 -0400

    Ajout des post 3D et Jeux dans la page projet

--- template-projets.php
+++ template-projets.php
@@ -5,8 +5,57 @@
  */
 ?>
 <?php get_header(); ?>
+
 <main>
-    <?php get_the_title(); ?>
-    <?php the_content(); ?>
+    <div class='projet__3d'>
+        <h1>3d</h1>
+        <?php
+          $category = get_queried_object();
+          $args = array(
+              'category_name' => '3D',
+              'orderby' => 'title',
+              'order' => 'ASC'
+            );
+            
+            $query = new WP_Query( $args );
+            if ( $query->have_posts() ) :
+                while ( $query->have_posts() ) : $query->the_post(); ?>
+                
+                
+                <?php the_content() ?>
+    
+            
+    
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+    </div>
+
+    <div class='projet__jeux'>
+        <h1>Jeux Vidéo</h1>
+        <?php
+          $category = get_queried_object();
+          $args = array(
+              'category_name' => 'Jeux',
+              'orderby' => 'title',
+              'order' => 'ASC'
+            );
+            
+            $query = new WP_Query( $args );
+            if ( $query->have_posts() ) :
+                while ( $query->have_posts() ) : $query->the_post(); ?>
+                
+                
+                <?php the_content() ?>
+    
+            
+    
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+    </div>
+   
+        
+      
 </main>
 <?php get_footer(); ?>
