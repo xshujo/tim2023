ef3390684f45b3c8e740492a11d303d386260def
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-31 09:09:55 -0400

    avancement css profs img, description, titre,etc

--- template-professeurs.php
+++ template-professeurs.php
@@ -7,40 +7,43 @@
 <?php
 get_header(); // Inclure l'en-tête du site 
 ?>
-<?php the_content(); ?>
-<div>
-    <div class="carousel">
-        <div class="zoneProfs">
+<div class="maria">
+    <?php the_content(); ?>
 
+    <div class="contenuCarr">
+        <div class="carousel">
+            <div class="zoneProfs">
+
+                <?php
+                query_posts('category_name=professeurs'); // Utilisez le nom de la catégorie "professeurs"
+
+                if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <figure class="prof__figure" id="fig_<?php the_id() ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </figure>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                <button class="precedent">
+                    < </button>
+                        <button class="suivant">></button>
+            </div>
+
+        </div>
+        <div class="description">
             <?php
-            query_posts('category_name=professeurs'); // Utilisez le nom de la catégorie "professeurs"
-
-            if (have_posts()) :
-                while (have_posts()) : the_post(); ?>
-                    <figure class="prof__figure" id="fig_<?php the_id() ?>">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                    </figure>
+            wp_reset_postdata();
+            if (have_posts()) :  while (have_posts()) : the_post(); ?>
+                    <section class="prof__section" id="des_<?php the_id(); ?>">
+                        <?php the_title(); ?>
+                        <figure class="prof__figure" id="fig_<?php the_id() ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </figure>
+                        <?php the_content(); ?>
+                    </section>
                 <?php endwhile; ?>
             <?php endif; ?>
-            <button class="precedent">
-                < </button>
-                    <button class="suivant">></button>
         </div>
-
-    </div>
-    <div class="description">
-        <?php
-        wp_reset_postdata();
-        if (have_posts()) :  while (have_posts()) : the_post(); ?>
-                <section class="prof__section" id="des_<?php the_id(); ?>">
-                    <?php the_title(); ?>
-                    <figure class="prof__figure" id="fig_<?php the_id() ?>">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                    </figure>
-                    <?php the_content(); ?>
-                </section>
-            <?php endwhile; ?>
-        <?php endif; ?>
     </div>
 </div>
 <?php
