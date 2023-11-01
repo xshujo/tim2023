0b890eceed50a1bb9b97805315f84cd3b54adc9f
ogregoryrivera <gregosorio24@gmail.com>
2023-10-31 21:23:06 -0400

    Ajout des commentaires

--- template-projets.php
+++ template-projets.php
@@ -4,24 +4,28 @@
  * Template name: Projets
  */
 ?>
+<!-- Ajouter le header -->
 <?php get_header(); ?>
 
 <main>
-
-<div class='bulle_1 bulle_1_position'></div>
+<!-- Création de bulles pour le style -->
+<div class='bulle_1 bulle_1_position'></div> 
+<div class='bulle_1_1 bulle_1_1_position'></div> 
 <div class='bulle_2 bulle_2_position'></div>
     
+<!-- Encadrer tous les sections à l'aide d'une div générale -->
 <div class='blockflex__projet'>
     <h1>Jeux Vidéo</h1>
     <div class='projet__general'>
 
 
-
+            <!-- Intégration de la catégorie Jeux -->
             <?php
               $category = get_queried_object();
               $args = array(
+                // Trouver la catégorie jeux et mettre en ordre de titre
                   'category_name' => 'Jeux',
-                  'orderby' => 'title',
+                  'orderby' => 'title', 
                   'order' => 'ASC'
                 );
 
@@ -31,10 +35,11 @@
 
                 <div class='carte__categorie'>
 
+                    <!-- Si la page a une catégorie, la rajouter -->
                     <?php if (has_post_thumbnail()) {
                             the_post_thumbnail('thumbnail');
                         } ?>
-
+                    <!-- Ajouter le contenu des post -->
                     <?php the_content() ?>
 
                 </div>
@@ -53,10 +58,11 @@
     <div class='projet__general'>
                         
 
-
+                <!-- Intégration de la catégorie Video -->        
                 <?php
                   $category = get_queried_object();
                   $args = array(
+                    // Trouver la catégorie video et mettre en ordre de titre
                       'category_name' => 'Video',
                       'orderby' => 'title',
                       'order' => 'ASC'
@@ -67,9 +73,11 @@
                         while ( $query->have_posts() ) : $query->the_post(); ?>
                         <div class='carte__categorie'>
 
+                            <!-- Si la page a une catégorie, la rajouter -->
                             <?php if (has_post_thumbnail()) {
                                     the_post_thumbnail('thumbnail');
                                 } ?>
+                            <!-- Ajouter le contenu des post -->    
                             <?php the_content() ?>
 
                         </div>
@@ -86,10 +94,11 @@
     <div class='projet__general'>
                                 
                                 
-                                
+            <!-- Intégration de la catégorie 3D -->                      
             <?php
               $category = get_queried_object();
               $args = array(
+                // Trouver la catégorie 3D et mettre en ordre de titre
                   'category_name' => '3D',
                   'orderby' => 'title',
                   'order' => 'ASC'
@@ -100,9 +109,11 @@
                     while ( $query->have_posts() ) : $query->the_post(); ?>
                     <div class='carte__categorie'>
 
+                        <!-- Si la page a une catégorie, la rajouter -->
                         <?php if (has_post_thumbnail()) {
                                 the_post_thumbnail('thumbnail');
                             } ?>
+                        <!-- Ajouter le contenu des post -->    
                         <?php the_content() ?>
 
                     </div>
@@ -115,10 +126,11 @@
     <div class='projet__general'>
                                 
                                 
-                                
+            <!-- Intégration de la catégorie Web -->                    
             <?php
               $category = get_queried_object();
               $args = array(
+                // Trouver la catégorie Web et mettre en ordre de titre
                   'category_name' => 'Web',
                   'orderby' => 'title',
                   'order' => 'ASC'
@@ -129,9 +141,11 @@
                     while ( $query->have_posts() ) : $query->the_post(); ?>
                     <div class='carte__categorie'>
 
+                        <!-- Si la page a une catégorie, la rajouter -->
                         <?php if (has_post_thumbnail()) {
                                 the_post_thumbnail('thumbnail');
                             } ?>
+                        <!-- Ajouter le contenu des post -->    
                         <?php the_content() ?>
 
                     </div>
@@ -144,10 +158,11 @@
     <div class='projet__general'>
                                 
                                 
-                                
+            <!-- Intégration de la catégorie Design -->                    
             <?php
               $category = get_queried_object();
               $args = array(
+                // Trouver la catégorie Design et mettre en ordre de titre
                   'category_name' => 'Design',
                   'orderby' => 'title',
                   'order' => 'ASC'
@@ -158,9 +173,11 @@
                     while ( $query->have_posts() ) : $query->the_post(); ?>
                     <div class='carte__categorie'>
 
-                        <?php if (has_post_thumbnail()) {
+                        <!-- Si la page a une catégorie, la rajouter -->
+                        <?php if (has_post_thumbnail()) {   
                                 the_post_thumbnail('thumbnail');
                             } ?>
+                        <!-- Ajouter le contenu des post -->
                         <?php the_content() ?>
 
                     </div>
@@ -175,4 +192,5 @@
          
       
 </main>
+<!-- Ajouter le footer -->
 <?php get_footer(); ?>
