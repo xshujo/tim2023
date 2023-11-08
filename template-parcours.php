d9e4286726828ae7e84b4546b245728fa7d75633
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-11-08 08:21:15 -0500

    Debut requête mobile-first

--- template-parcours.php
+++ template-parcours.php
@@ -9,6 +9,14 @@
 <main class="contenu_parcours">
     <?php get_the_title(); ?>
     <?php the_content(); ?>
+    <div class="encadre_page"></div>
+    <div class="degrades degrade_1"></div>
+    <div class="degrades degrade_2"></div>
+    <div class="degrades degrade_3"></div>
+    <div class="bulle_modele_1 bulle_m1_position_1"></div>
+    <div class="bulle_modele_1 bulle_m1_position_2"></div>
+    <div class="bulle_modele_2 bulle_m2_position_1"></div>
+    <div class="bulle_modele_2 bulle_m2_position_2"></div>
    
 </main>
 
