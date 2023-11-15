6777e3e5dd89f9173e0883ceeb782a858287b12e
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-11-14 20:10:36 -0500

    Requête mobile first partie 3

--- template-parcours.php
+++ template-parcours.php
@@ -21,9 +21,30 @@
     <div class="bulle_modele_3 bulle_m3_position_2"></div>
     <div class="bulle_modele_4 bulle_m4_position_1"></div>
     <div class="bulle_modele_4 bulle_m4_position_2"></div>
-    
-   
-   
+    <button id="scroll-to-top" class="scroll-to-top-button" title="Scroll to Top">↑</button>
+
+    <script>
+        // Get a reference to the button
+        const scrollToTopButton = document.getElementById('scroll-to-top');
+
+        // Show the button when the page is scrolled down
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset > 300) {
+                scrollToTopButton.style.display = 'block';
+            } else {
+                scrollToTopButton.style.display = 'none';
+            }
+        });
+
+        // Scroll to the top when the button is clicked
+        scrollToTopButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth', // Smooth scrolling animation
+            });
+        });
+    </script>
+
 </main>
 
 <?php get_footer(); ?>
