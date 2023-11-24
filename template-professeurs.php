0edd5986bf7c3ccfadd6efd5b1ee0d40bead642c
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-24 08:35:31 -0500

    Ajout design arriere-plan page prof

--- template-professeurs.php
+++ template-professeurs.php
@@ -7,20 +7,18 @@
 <?php
 get_header();
 ?>
+<?php the_content(); ?>
+
 <!----- CONTENUE AFFICHER DANS LA PAGE PROFESSEURS ---->
 <div class="contenuProf">
     <!----- Titre principale page professeurs ---->
 
 
 
-    <!-----  ZONE DEGRADE EN ARRIERE-PLAN ---->
-    <div class="encadre_page"></div>
-    <div class="degrades degrade_1"></div>
-    <div class="degrades degrade_2"></div>
-    <div class="degrades degrade_3"></div>
-    <div class="bulle_modele_1 bulle_m1_position_1"></div>
-    <div class="bulle_modele_2 bulle_m2_position_1"></div>
-    <div class="bulle_modele_2 bulle_m2_position_2"></div>
+    <!-----  ZONE DESIGN EN ARRIERE-PLAN ---->
+    <div class="bulleBg"></div>
+    <div class="bulleBg_deux"></div>
+    <div class="barreBlanc"></div>
 </div>
 <!----- ///////////////////////////////////////////////////////// ---->
 <?php
