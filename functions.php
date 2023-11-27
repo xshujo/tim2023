459855a5755251480bf45c242fdb5b25c8a41c7a
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-27 01:50:39 -0500

    retrait fichier inutile

--- functions.php
+++ functions.php
@@ -8,20 +8,6 @@ function ajouter_styles()
         array(), // Les fichiers CSS qui dépendent de style.css
         filemtime(get_template_directory() . '/style.css') // Version de notre style.css
     );
-    wp_enqueue_script(
-        'script-description-profs',
-        get_template_directory_uri() . '/code/profs.js',
-        array(),
-        filemtime(get_template_directory() . '/code/profs.js'), // Version de notre style.css
-        true
-    );
-    wp_enqueue_script(
-        'script-carrousel-image',
-        get_template_directory_uri() . '/code/carousel.js',
-        array(),
-        filemtime(get_template_directory() . '/code/carousel.js'),
-        true
-    );
 }
 add_action('wp_enqueue_scripts', 'ajouter_styles');
 
