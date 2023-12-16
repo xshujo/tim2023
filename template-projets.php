193f1874359e10e7834287a46a9f570f9e8dd08b
ogregoryrivera <gregosorio24@gmail.com>
2023-12-16 16:31:33 -0500

    avancement

--- template-projets.php
+++ template-projets.php
@@ -8,13 +8,6 @@
 <?php get_header(); ?>
 
 <main>
-  <!-- Création de bulles pour le style -->
-  <div class='bulle_1 bulle_1_position'></div>
-  <div class='bulle_1_1 bulle_1_1_position'></div>
-  <div class='bulle_2 bulle_2_position'></div>
-  <div class='degrades degrade_1'></div>
-  <div class='degrades degrade_2'></div>
-  <div class='degrades degrade_3'></div>
 
   <h1 class="text__page__projet">Projet</h1>
 
@@ -22,8 +15,13 @@
   <div class='blockflex__projet'>
 
     <h1 class='jeux'>Jeux Vidéo</h1>
-    <div class='projet__general div1'>
 
+    <div class="projet__jeux">
+    <div class="encadre">
+
+
+      <div class='projet__general projet__general1'>
+      
 
       <!-- Intégration de la catégorie Jeux -->
       <?php
@@ -39,20 +37,28 @@
       if ($query->have_posts()) :
         while ($query->have_posts()) : $query->the_post(); ?>
 
-          <div class='carte__categorie'>
+
+          
+          <div class='carte__categorie carte__categorie1'>
+            
 
             <!-- Si la page a une catégorie, la rajouter -->
             <?php if (has_post_thumbnail()) {
-              the_post_thumbnail('thumbnail');
+              the_post_thumbnail('thumbnail', ['class' => 'img__categorie']);
             } ?>
             <!-- Ajouter le contenu des post -->
-            <?php the_content() ?>
+            
+              <?php the_content() ?>
+            
+          </div>  
 
-          </div>
 
         <?php endwhile; ?>
       <?php endif;
       wp_reset_postdata(); ?>
+    
+           
+    
     </div>
 
 
