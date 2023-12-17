825bfe9bb5d38cc4299403a76d18ad9f5e88ae69
Stephanie Guerrier <stephanieguerrier3@hotmail.com>
2023-12-17 11:36:08 -0500

    Ajout modification page professeur fais a partir de siteground

--- template-professeurs.php
+++ template-professeurs.php
@@ -8,15 +8,36 @@
 get_header();
 ?>
 <!----- CONTENUE AFFICHER DANS LA PAGE PROFESSEURS ---->
-<div class="contenuProf">
+<main class="contenuProf">
     <?php the_content(); ?>
 
+  <button id="scroll-to-top" class="scroll-to-top-button" title="Scroll to Top">↑</button>
 
     <!-----  ZONE DESIGN EN ARRIERE-PLAN ---->
-    <div class="bulleBg"></div>
-    <div class="bulleBg_deux"></div>
     <div class="barreBlanc"></div>
-</div>
+    <script>
+    / Get a reference to the button
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    // Show the button when the page is scrolled down
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 30) {
+        scrollToTopButton.style.display = 'block';
+      } else {
+        scrollToTopButton.style.display = 'none';
+      }
+    });
+
+    // Scroll to the top when the button is clicked
+    scrollToTopButton.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // Smooth scrolling animation
+      });
+    });
+    </script>
+</main>
+
 <!----- ///////////////////////////////////////////////////////// ---->
 <?php
 get_footer();
